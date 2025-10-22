97372b59d758dfd21d863bfa96535480a9f6fbeb
piyushsinghnegi <piyushsinghnegi04@gmail.com>
2025-10-22 19:45:04 +0530

    changes done

--- student_performance_analyzer.c
+++ student_performance_analyzer.c
@@ -1,30 +1,40 @@
 #include <stdio.h>
 #include <string.h>
 
+#define SUBJECTS 3
+#define MAX_STUDENTS 1000000
+#define MAX_PERFORMANCE 10
+#define MAX_NAME 100
+
+const int gradeA = 85;
+const int gradeB = 70;
+const int gradeC = 50;
+const int gradeD = 35;
+
 struct Student {
     int roll;
-    char name[100];
-    int marks[3];
+    char name[MAX_NAME];
+    int marks[SUBJECTS];
     int total;
     float average;
     char grade;
-    char performance[10];
+    char performance[MAX_PERFORMANCE];
 };
 
 void getGrade(float avg, char *grade, char *performance) {
-    if (avg >= 85) {
+    if (avg >= gradeA) {
         *grade = 'A'; 
         strcpy(performance, "*****");
 
-    } else if (avg >= 70) {
+    } else if (avg >= gradeB) {
         *grade = 'B';
         strcpy(performance, "****");
 
-    } else if (avg >= 50) {
+    } else if (avg >= gradeC) {
         *grade = 'C';
         strcpy(performance, "***");
 
-    } else if (avg >= 35) {
+    } else if (avg >= gradeD) {
         *grade = 'D';
         strcpy(performance, "**");
 
@@ -35,58 +45,61 @@ void getGrade(float avg, char *grade, char *performance) {
     }
 }
 
-void result(struct Student std[], int i, int no) {
-    if (i == no) {
+void result(struct Student std[], int index, int totalStudents) {
+    if (index == totalStudents) {
         return;
     }
-    printf("%d ", std[i].roll);
-    result(std, i + 1, no);
+    printf("%d ", std[index].roll);
+    result(std, index + 1, totalStudents);
 }
 
 int main() {
-    long long temp;
+    long long inputStudentNo;
     
     printf("Enter number of students: ");
     
-    if (scanf("%lld", &temp) != 1 || temp <= 0 || temp > 1000000) {
+    if (scanf("%lld", &inputStudentNo) != 1 || inputStudentNo <= 0 || inputStudentNo > MAX_STUDENTS) {
         printf("Error: Invalid no of students \n");
         return 0;
     }
-    int no = (int)temp;
+    int studentCount = (int)inputStudentNo;
 
-    struct Student std[no];
+    struct Student std[studentCount];
 
     // sorting 
-    for (int i = 0; i < no; i++) {
-        scanf("%d %s %d %d %d", &std[i].roll, std[i].name, &std[i].marks[0], &std[i].marks[1], &std[i].marks[2]);
-        std[i].total = std[i].marks[0] + std[i].marks[1] + std[i].marks[2];
+    for (int stdIndex = 0; stdIndex < studentCount; stdIndex++) {
+
+        scanf("%d %s %d %d %d", &std[stdIndex].roll, std[stdIndex].name, &std[stdIndex].marks[0], &std[stdIndex].marks[1], &std[stdIndex].marks[2]);
+        std[stdIndex].total = std[stdIndex].marks[0] + std[stdIndex].marks[1] + std[stdIndex].marks[2];
         
-        std[i].average = std[i].total / 3.0;
-        getGrade(std[i].average, &std[i].grade, std[i].performance);
+        std[stdIndex].average = std[stdIndex].total / 3.0;
+        getGrade(std[stdIndex].average, &std[stdIndex].grade, std[stdIndex].performance);
     }
 
-    for (int i = 0; i < no - 1; i++) {
-        for (int j = 0; j < no - i - 1; j++) {
-            if (std[j].roll > std[j + 1].roll) {
-                struct Student temp = std[j];
-                std[j] = std[j + 1];
-                std[j + 1] = temp;
+    for (int outerIndex = 0; outerIndex < studentCount - 1; outerIndex++) {
+
+        for (int innerIndex = 0; innerIndex < studentCount - outerIndex - 1; innerIndex++) {
+        
+            if (std[innerIndex].roll > std[innerIndex + 1].roll) {
+                struct Student tempStd = std[innerIndex];
+                std[innerIndex] = std[innerIndex + 1];
+                std[innerIndex + 1] = tempStd;
             }
         }
     }
 
     printf("\n****** Student Results ******\n");
-    for (int i = 0; i < no; i++) {
-        printf("Roll: %d\n", std[i].roll);
-        printf("Name: %s\n", std[i].name);
-        printf("Total: %d\n", std[i].total);
-        printf("Average: %.2f\n", std[i].average);
-        printf("Grade: %c\n", std[i].grade);
-        printf("Performance: %s\n\n", std[i].performance);
+    for (int index = 0; index < studentCount; index++) {
+        printf("Roll: %d\n", std[index].roll);
+        printf("Name: %s\n", std[index].name);
+        printf("Total: %d\n", std[index].total);
+        printf("Average: %.2f\n", std[index].average);
+        printf("Grade: %c\n", std[index].grade);
+        printf("Performance: %s\n\n", std[index].performance);
     }
 
     printf("List of Roll Numbers (via recursion): ");
-    result(std, 0 ,no);
+    result(std, 0 ,studentCount);
     printf("\n");
 
     return 0;
